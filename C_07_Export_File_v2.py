43596026f536fdbaefb33b0a847e53b4e0727f70
JackPringle <101750172+JackPringle@users.noreply.github.com>
2024-03-14 14:16:30 +1300

    Writes variable and fixed costs to file

--- C_07_Export_File_v2.py
+++ C_07_Export_File_v2.py
@@ -24,9 +24,7 @@ fixed_txt = pandas.DataFrame.to_string(fixed_frame)
 product_name = "Custom Mugs"
 profit_target = "$100.00"
 required_sales = "$200.00"
-recommended_price = "the recommended price is $50.00"
-
-print(variable_frame)
+recommended_price = "the recommended price is $5.00"
 
 to_write = [product_name, variable_txt, fixed_txt, profit_target, required_sales, recommended_price]
 
@@ -40,8 +38,10 @@ for item in to_write:
     text_file.write(item)
     text_file.write("\n\n")
 
+# Close file
+text_file.close()
 
-
-
-
-
+# Print stuff
+for item in to_write:
+    print(item)
+    print()
